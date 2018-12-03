2ddd5789686a72a4b44e262ae325276f7c922218
Alexis Araujo <ala1289@gmail.com>
2018-12-03 12:28:25 -0600

    first commit

---
+++ make-chart.js
@@ -0,0 +1,23 @@
+const context = document.querySelector('#chart').getContext('2d')
+
+function makeChart(type, datasetList, labels, colors) {
+  return new Chart(context, {
+    type: type,
+      data: {
+          labels: labels,
+          datasets: datasetList
+      },
+      options: {
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true
+            }
+          }]
+        }
+      }
+  })
+}
+
+export default makeChart
